1f5028bad049326ad1ca57ef463916e642eac8e9
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-03-29 16:06:46 +0530

    Time: 586 ms (40.81%) | Memory: 78.1 MB (22.21%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,43 +1,58 @@
 class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-
+    def __init__(self,key,val):
+        self.key=key
+        self.val=val
+        self.next=self.prev=None
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # map key to node
+        self.capacity=capacity
+        self.cache={}
 
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.left,self.right=Node(0,0),Node(0,0)
+        self.left.next,self.right.prev=self.right,self.left
 
-    # remove node from list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
+    def remove(self,node):
+        prev=node.prev
+        next=node.next
 
-    # insert node at right
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        prev.next=next
+        next.prev=prev
+
+    def insert(self,node):
+        prev=self.right.prev
+        next=self.right
+
+        next.prev=node
+
+        node.next=next
+        node.prev=prev
+
+        prev.next=node
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
+
             return self.cache[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        node=Node(key,value)
+        self.insert(node)
+        self.cache[key]=node
 
-        if len(self.cache) > self.cap:
-            # remove from the list and delete the LRU from hashmap
-            lru = self.left.next
+        if len(self.cache)>self.capacity:
+            lru=self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
