a39ec89603ea4345dc3df5a7cb24e53b179e1e02
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-05-05 12:11:30 +0530

    Time: 264 ms (30.75%) | Memory: 19 MB (45.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,31 +4,22 @@ class Solution:
             return 0
 
         rows, cols = len(grid), len(grid[0])
-        visited=set()
         islands=0
+        #using dfs
 
-        def bfs(r,c):
-            q = deque()
-            visited.add((r,c))
-            q.append((r,c))
-           
-            directions= [[1,0],[-1,0],[0,1],[0,-1]]
-            while q:
-                row,col = q.popleft()
-                
-               
-                for dr,dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                       
-                        q.append((r , c ))
-                        visited.add((r, c ))
-
+        def dfs(r,c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c]=='0':
+                return
+            grid[r][c]='0'
+            dfs(r+1,c)
+            dfs(r,c+1)
+            dfs(r-1,c)
+            dfs(r,c-1)
+        
         for r in range(rows):
             for c in range(cols):
-               
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r,c)
-                    islands +=1 
-
+                if grid[r][c]=='1':
+                    islands+=1
+                    dfs(r,c)
         return islands
+            
