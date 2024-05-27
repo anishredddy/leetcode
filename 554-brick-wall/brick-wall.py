6014c02894c7796f07b5b061b0e19ea7e3e27c9b
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-05-27 12:47:52 +0530

    Time: 165 ms (17.34%) | Memory: 21.4 MB (67.39%) - LeetSync

--- 554-brick-wall/brick-wall.py
+++ 554-brick-wall/brick-wall.py
@@ -1,13 +1,11 @@
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
         gap=defaultdict(int)
+        maxEdges=0
         for i in range(len(wall)):
             prev=0
             for j in range(len(wall[i])-1):
                 prev=prev+wall[i][j]
                 gap[prev]+=1
-        n=len(wall)
-        res=float('inf')
-        for val in gap.values():
-            res=min(res,n-val)
-        return n if res==float('inf') else res
+                maxEdges=max(gap[prev],maxEdges)
+        return len(wall)-maxEdges
