0302fb3f5ace83d867b977eb2360c3f697fea949
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-07-11 23:22:04 +0530

    Time: 42 ms (16.58%) | Memory: 16.5 MB (81.94%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,21 +1,26 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
         stack=[]
-        for char in s:
-            if char==")":
-                temp=""
-                while stack:
-                    ch=stack.pop()
-                    if ch=="(":
-                        break
-                    temp+=ch
-                for char in temp:
-                    stack.append(char)
-                
-            else:
-                stack.append(char)
-        return "".join(stack)
+        pairs={}
 
+        for i in range(len(s)):
+            if s[i]=="(":
+                stack.append(i)
+            elif s[i]==")":
+                j=stack.pop()
+                pairs[i]=j
+                pairs[j]=i
+
+        i,dir=0,1
+        res=[]
+        while i<len(s):
+            if s[i]=="(" or s[i]==")":
+                i=pairs[i]
+                dir=-dir
+            else:
+                res.append(s[i])
+            i+=dir
+        return "".join(res)
         # ed et co el
         # ed octe el
         # leetcode
