1352a49d5910a8d715d8aff2d253150d68fdd4bd
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-07-14 22:43:43 +0530

    Time: 4205 ms (5.46%) | Memory: 30.1 MB (9.50%) - LeetSync

---
+++ 726-number-of-atoms/number-of-atoms.py
@@ -0,0 +1,56 @@
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        stack=[]
+        i=0
+        while i<len(formula):
+            if formula[i]==")":
+                count=""
+                while i+1<len(formula) and formula[i+1].isnumeric():
+                    count+=formula[i+1]
+                    i+=1
+                if count=="":
+                    count=1
+                else:
+                    count=int(count)
+                #now get string from stack
+                s=""
+                while stack:
+                    char=stack.pop()
+                    if char=="(":
+                        break
+                    s=char+s
+
+                stack.append(s*count)
+            else:
+                stack.append(formula[i])
+            i+=1
+        
+        formula="".join(stack)
+        d=defaultdict(int)
+
+        i=count=0
+        
+        while i<len(formula):
+            if formula[i].isupper():
+                ele=formula[i]
+                if i+1<len(formula) and formula[i+1].islower():
+                    i+=1
+                    ele+=formula[i]
+                count=""
+                while i+1<len(formula) and formula[i+1].isnumeric():
+                    count+=formula[i+1]
+                    i+=1
+
+                if count=="":
+                    count=1
+                else:
+                    count=int(count)
+                d[ele]+=count
+            i+=1
+        res=""
+        print(d)
+        for key in sorted(d.keys()):
+            res+=key
+            if d[key]!=1:
+                res+=str(d[key])
+        return res
