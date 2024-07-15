5d1d7763816caacb7b3f08fd46b4bd64ea8cc514
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-07-16 00:01:44 +0530

    Time: 39 ms (41.62%) | Memory: 16.7 MB (17.57%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,21 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s=s.strip()
-        stack=[]
-
-        curr_s=""
-        prev=""
-
-        for char in s:
-            if char==" ":
-                if prev==" ":
-                    continue
-                else:
-                    stack.append(curr_s)
-                    curr_s=""
-            else:
-                curr_s+=char
-            prev=char
-        if curr_s!="":
-            stack.append(curr_s)
-        return " ".join(stack[::-1])
+        return " ".join(s.split()[::-1])
