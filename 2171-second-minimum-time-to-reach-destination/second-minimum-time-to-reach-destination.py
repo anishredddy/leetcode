e4e8524b0b9e16c9d8e191238dc49816b94ff077
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-07-30 17:11:05 +0530

    Time: 1794 ms (50.58%) | Memory: 26.1 MB (89.96%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.py
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.py
@@ -1,55 +1,35 @@
 class Solution:
 
     def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
-
-        g = [[] for _ in range(n + 1)]
-
-        for u, v in edges:
-
-            g[u].append(v)
-
-            g[v].append(u)
-
+        graph=defaultdict(list)
+        for i in range(len(edges)):
+            node1,node2=edges[i]
+            graph[node1].append(node2)
+            graph[node2].append(node1)
         
+        count=0
+        start=1
 
-        q = deque([(1, 1)])
 
-        dist1 = [-1] * (n + 1)
+        q=deque()
+        q.append(1)
 
-        dist2 = [-1] * (n + 1)
-
-        dist1[1] = 0
+        currTime=0
+        visit=defaultdict(list)
 
         while q:
-
-            x, freq = q.popleft()
-
-            t = dist1[x] if freq == 1 else dist2[x]
-
-            if (t // change) % 2:
-
-                t = change * (t // change + 1) + time
-
-            else:
-
-                t += time
-
-            for y in g[x]:
-
-                if dist1[y] == -1:
-
-                    dist1[y] = t
-
-                    q.append((y, 1))
-
-                elif dist2[y] == -1 and dist1[y] != t:
-
-                    if y == n:
-
-                        return t
-
-                    dist2[y] = t
-
-                    q.append((y, 2))
-
-        return 0
+            for i in range(len(q)):
+                node=q.popleft()
+                if node==n:
+                    if count:
+                        return currTime
+                    count+=1
+                for nei in graph[node]:
+                    nei_times=visit[nei]
+                    if len(nei_times)==0 or (len(nei_times)==1 and nei_times[0]!=currTime):
+                        q.append(nei)
+                        visit[nei].append(currTime)
+                    
+            if (currTime//change)%2==1:
+                currTime+=change-(currTime%change)
+            currTime+=time
