cf47a1f0fe358efb0a07d4e7ce320b3aa38502ac
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-09-15 20:31:08 +0530

    Time: 217 ms (52.32%) | Memory: 18.5 MB (79.53%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -1,27 +1,14 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        # Create a boolean list to mark which characters are allowed
-        is_allowed = [False] * 26
-
-        # Mark all characters in 'allowed' as True in the is_allowed list
-        for char in allowed:
-            is_allowed[ord(char) - ord("a")] = True
-
-        consistent_count = 0
-
-        # Iterate through each word in the words list
+        bit_mask=0
+        for c in allowed:
+            bit=1<<(ord(c)-ord('a'))
+            bit_mask|=bit
+        res=len(words)
         for word in words:
-            is_consistent = True
-
-            # Check each character of the current word
-            for char in word:
-                # If any character is not allowed, mark as inconsistent and break
-                if not is_allowed[ord(char) - ord("a")]:
-                    is_consistent = False
+            for c in word:
+                bit=1<<(ord(c)-ord('a'))
+                if bit&bit_mask==0:
+                    res-=1
                     break
-
-            # If the word is consistent, increment the count
-            if is_consistent:
-                consistent_count += 1
-
-        return consistent_count
+        return res
