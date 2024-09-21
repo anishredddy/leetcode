85fc0740c955fdb5f325ce66112b7f193cabe422
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-09-21 16:09:41 +0530

    Time: 135 ms (18.06%) | Memory: 24.9 MB (20.22%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,16 +1,15 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         res=[]
-        nums=[0,1,2,3,4,5,6,7,8,9]
         def dfs(i):
             if i>n:
                 return
             res.append(i)
             # if i not in res:
             #     res.append(i)
-            for num in nums:
+            for num in range(10):
                 dfs(i*10+num)
         for i in range(1,10):
             dfs(i)
-            
+
         return res
