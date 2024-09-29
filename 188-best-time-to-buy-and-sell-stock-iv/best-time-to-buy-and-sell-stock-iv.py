539ab781563f6443104f19f82769c055019eebc2
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-09-29 17:50:23 +0530

    Time: 92 ms (76.09%) | Memory: 17.9 MB (58.48%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -1,22 +1,18 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        mem={}
-        def dp(i,holding,remain):
-            if (i,holding,remain) in mem:
-                return mem[(i,holding,remain)]
-            if i==len(prices) or remain==0:
-                return 0
-            
-            #skip
-            ans=dp(i+1,holding,remain)
+        if not prices or k==0:
+            return 0
+        if k>=len(prices)//2:
+            res=0
+            for i in range(1,len(prices)):
+                if(prices[i-1]<prices[i]):
+                    res+=prices[i]-prices[i-1]
+            return res
+        dp = [[0] * len(prices) for _ in range(k + 1)]
 
-            if holding:
-                #sell
-                ans=max(ans,prices[i]+dp(i+1,False,remain-1))
-            else:
-                #buy
-                ans=max(ans,-prices[i]+dp(i+1,True,remain))
-            mem[(i,holding,remain)]=ans
-            return ans
-
-        return dp(0,False,k)
+        for i in range(1,k+1):
+            diff=-prices[0]
+            for j in range(1,len(prices)):
+                dp[i][j]=max(dp[i][j-1],prices[j]+diff)
+                diff=max(diff,dp[i-1][j]-prices[j])
+        return dp[k][len(prices)-1]
