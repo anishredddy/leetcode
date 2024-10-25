0c0ec09c8d1f5f1d6cd55b166f4e2dca4cd36318
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-10-25 15:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (58.31%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -1,25 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def flipEquiv(
-        self, root1: Optional[TreeNode], root2: Optional[TreeNode]
-    ) -> bool:
-        # Both trees are empty
-        if not root1 and not root2:
-            return True
-        # Just one of the trees is empty
-        if not root1 or not root2:
-            return False
-        # Corresponding values differ
-        if root1.val != root2.val:
-            return False
-
-        # Check if corresponding subtrees are flip equivalent
-        no_swap = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(
-            root1.right, root2.right
-        )
-
-        # Check if opposite subtrees are flip equivalent
-        swap = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(
-            root1.right, root2.left
-        )
-
-        return no_swap or swap
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(root1,root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is None or root2 is None:
+                return False
+            if root1.val!=root2.val:
+                return False
+            return (dfs(root1.left,root2.left) and dfs(root1.right,root2.right)) or (dfs(root1.left,root2.right) and dfs(root1.right,root2.left))
+            # if root1.left==root2.left and root1.right==root2.right:
+            #     return dfs(root1.left,root2.left) and dfs(root1.right,root2.right)
+            # elif root1.left==root2.right and root1.right==root2.left:
+            #     return dfs(root1.left,root2.right) and dfs(root1.right,root2.left)
+            # else:
+            #     return False
+        return dfs(root1,root2)
