068d7cea925418a81e199b717b6d8727a07b1d88
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-10-25 20:50:59 +0530

    Time: 225 ms (63.52%) | Memory: 16.6 MB (68.34%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,43 +1,24 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_or_value = 0
-        n = len(nums)
+        res=0
+        n=len(nums)
 
-        # Calculate the maximum OR value
+        bitwise=0
         for num in nums:
-            max_or_value |= num
+            bitwise=bitwise|num
 
-        # Initialize memo with -1
-        memo = [[-1] * (max_or_value + 1) for _ in range(n)]
+        def dfs(ind,ans):
+            nonlocal res
+            if ind==n:
+                return
+            #include
+            new_ans=ans|nums[ind]
+            if new_ans==bitwise:
+                res+=1
+            
 
-        return self._count_subsets_recursive(nums, 0, 0, max_or_value, memo)
-
-    def _count_subsets_recursive(
-        self,
-        nums: List[int],
-        index: int,
-        current_or: int,
-        target_or: int,
-        memo: List[List[int]],
-    ) -> int:
-        # Base case: reached the end of the array
-        if index == len(nums):
-            return 1 if current_or == target_or else 0
-
-        # Check if the result for this state is already memoized
-        if memo[index][current_or] != -1:
-            return memo[index][current_or]
-
-        # Don't include the current number
-        count_without = self._count_subsets_recursive(
-            nums, index + 1, current_or, target_or, memo
-        )
-
-        # Include the current number
-        count_with = self._count_subsets_recursive(
-            nums, index + 1, current_or | nums[index], target_or, memo
-        )
-
-        # Memoize and return the result
-        memo[index][current_or] = count_without + count_with
-        return memo[index][current_or]
+            dfs(ind+1,new_ans)
+            #dont include
+            dfs(ind+1,ans)
+        dfs(0,0)
+        return res
