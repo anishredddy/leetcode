b69645d9411f18739cc7c2b301ed5be4a6df9e73
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-10-25 21:01:39 +0530

    Time: 5 ms (98.16%) | Memory: 16.8 MB (41.79%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,24 +1,8 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        res=0
-        n=len(nums)
-
-        bitwise=0
+        res=defaultdict(int)
         for num in nums:
-            bitwise=bitwise|num
-
-        def dfs(ind,ans):
-            nonlocal res
-            if ind==n:
-                return
-            #include
-            new_ans=ans|nums[ind]
-            if new_ans==bitwise:
-                res+=1
-            
-
-            dfs(ind+1,new_ans)
-            #dont include
-            dfs(ind+1,ans)
-        dfs(0,0)
-        return res
+            res[0]=1
+            for key,val in [*res.items()]:
+                res[key|num]+=val
+        return res[max(res.keys())]
