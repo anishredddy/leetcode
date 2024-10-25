f0a57c99f40670e0afb44f8cb3b266b3f1f7c417
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-10-25 21:36:43 +0530

    Time: 523 ms (21.74%) | Memory: 21.8 MB (33.41%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,19 +1,16 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        invert_count = 0
-        len = (1 << n) - 1  # Length of Sn is 2^n - 1
-
-        while k > 1:
-            # If k is in the middle, return based on inversion count
-            if k == len // 2 + 1:
-                return "1" if invert_count % 2 == 0 else "0"
-
-            # If k is in the second half, invert and mirror
-            if k > len // 2:
-                k = len + 1 - k  # Mirror position
-                invert_count += 1  # Increment inversion count
-
-            len //= 2  # Reduce length for next iteration
-
-        # For the first position, return based on inversion count
-        return "0" if invert_count % 2 == 0 else "1"
+        def findK(si,i):
+            rev=""
+            for char in si:
+                if char=="1":
+                    rev+="0"
+                else:
+                    rev+="1"
+            sii=si+"1"+rev[::-1]
+            if i+1==n:
+                return sii
+            return findK(sii,i+1)
+        if n==1:
+            return "0"
+        return findK("0",1)[k-1]
