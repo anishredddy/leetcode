fe90ad76c10a0091fe647db8c1d7b0627f380cbf
anishredddy <131188230+anishredddy@users.noreply.github.com>
2024-11-02 12:34:01 +0530

    Time: 11 ms (76.01%) | Memory: 17.1 MB (28.13%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -1,48 +1,24 @@
+from collections import deque
+
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
-        i = 0
-        count = {'R': 0, 'D': 0}
-        for char in senate:
-            count[char] += 1
-    
-        senate = list(senate)
-        while count['R']  > 0 and count['D'] > 0:
-            if senate[i] == "D":
-                flag = False
-                for j in range(i+1, len(senate)): #check right first
-                    if senate[j] == 'R':
-                        senate[j] = 'X'
-                        count['R'] -= 1
-                        flag = True
-                        break
-                if not flag: #if not found in right check left
-                    for j in range(0, i):
-                        if senate[j] == 'R':
-                            senate[j] = 'X'
-                            count['R'] -= 1
-                            break
-            elif senate[i] == "R":
-                flag = False
-                for j in range(i+1, len(senate)):
-                    if senate[j] == 'D':
-                        senate[j] = 'X'
-                        count['D'] -= 1
-                        flag = True
-                        break
-                if not flag:
-                    for j in range(0, i):
-                        if senate[j] == 'D':
-                            senate[j] = 'X'
-                            count['D'] -= 1
-                            break
-            if i == len(senate) - 1: #if on last index start again from 0
-                i = 0
+        radiant = deque()
+        dire = deque()
+        for i, s in enumerate(senate):
+            if s == 'R':
+                radiant.append(i)
             else:
-                i += 1
-        for i, v in count.items():
-            if v > 0:
-                if i == 'R':
-                    return "Radiant"
-                return "Dire"
-                
+                dire.append(i)
 
+        n = len(senate)
+
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+
+        return "Radiant" if radiant else "Dire"
