5579b1dbe18cbebe341b3d48be4b781c2c063b3f
anishredddy <131188230+anishredddy@users.noreply.github.com>
2025-08-20 22:48:00 +0530

    Time: 11 ms (72.32%) | Memory: 17.8 MB (86.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        if len(s)!=len(t):
+            return False
+        arr1=[0]*26
+        arr2=[0]*26
+        for i in range(len(s)):
+            arr1[ord(s[i])-ord('a')]+=1
+            arr2[ord(t[i])-ord('a')]+=1
+        return arr1==arr2
